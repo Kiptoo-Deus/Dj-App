0213b035d88ba0bb7ae33c76f219a7435be49abc
Joel Kiptoo <deusjoel150@gmail.com>
2023-05-11 12:51:11 +0300

    Update AudioPlayerData.cpp

--- Source/AudioPlayer/Data/AudioPlayerData.cpp
+++ Source/AudioPlayer/Data/AudioPlayerData.cpp
@@ -14,25 +14,6 @@ AudioPlayerData::AudioPlayerData()
 {
     audioFormatManager.registerBasicFormats();
 }
-//bool AudioPlayerData::loadFile()
-//{
-//    DBG("Load button clicked");
-//
-//    juce::File musicFile{ "C:\\lifted.mp3" };
-//    jassert(musicFile.exists());
-//
-//    auto* r = audioFormatManager.createReaderFor(juce::File("C:\\lifted.mp3"));
-//    std::unique_ptr<juce::AudioFormatReader>reader(r);
-//    jassert(reader != nullptr);
-//    auto numSamples = static_cast<int>(reader->lengthInSamples);
-//
-//    audioSourceBuffer.setSize(reader->numChannels, numSamples);
-//    jassert(numSamples > 0 && reader->numChannels > 0);
-//
-//    //file loaded succesufully?
-//   return reader->read(&audioSourceBuffer, 0, numSamples, 0, true, true);
-//
-//}
 bool AudioPlayerData::loadFile()
 {
     DBG("Load button clicked");
@@ -40,42 +21,42 @@ bool AudioPlayerData::loadFile()
     juce::File musicFile{ "C:\\lifted.mp3" };
     jassert(musicFile.exists());
 
-    auto* reader = audioFormatManager.createReaderFor(musicFile);
-    if (reader == nullptr) {
-        return false;
-    }
-   
-    
+    auto* r = audioFormatManager.createReaderFor(juce::File("C:\\lifted.mp3"));
+    std::unique_ptr<juce::AudioFormatReader>reader(r);
+    jassert(reader != nullptr);
     auto numSamples = static_cast<int>(reader->lengthInSamples);
 
     audioSourceBuffer.setSize(reader->numChannels, numSamples);
     jassert(numSamples > 0 && reader->numChannels > 0);
 
+    //file loaded succesfully?
     reader->read(&audioSourceBuffer, 0, numSamples, 0, true, true);
     return true;
 }
-
 
 void AudioPlayerData::processAudio(const juce::AudioSourceChannelInfo& bufferToFill)
 {
     auto* buffer = bufferToFill.buffer;
 
+
+
     //copy buffer size number of samples ofaudio source buffer to main buffer
     jassert(buffer->getNumChannels() == audioSourceBuffer.getNumChannels());
+
+
+
     for (int ch = 0; ch < buffer->getNumChannels(); ch++)
     {
         buffer->copyFrom(ch, 0, audioSourceBuffer, ch, readPosition, buffer->getNumSamples());
+        buffer->applyGain(ch, 0, buffer->getNumSamples(), rawGain);
     }
     readPosition += buffer->getNumSamples();
 
 }
 
-    
-void AudioPlayerData::setGain(float newGain)
+   void AudioPlayerData::setDecibelValue(float value)
 {
-    auto numSample = audioSourceBuffer.getNumSamples();
-    for (int ch = 0; ch < audioSourceBuffer.getNumChannels(); ch++)
-        {
-            audioSourceBuffer.applyGain(ch, 0, numSample, newGain);//modified this line
-        };
+   
+     rawGain = juce::Decibels::decibelsToGain(value);
+  
 }
