ba552c01d5c6ef8b84cbecc39bebbdbe21a34db6
Joel Kiptoo <deusjoel150@gmail.com>
2023-08-23 12:36:04 +0300

    Added Artist Name Label

--- Source/AudioPlayer/UI/AudioPlayerUI.h
+++ Source/AudioPlayer/UI/AudioPlayerUI.h
@@ -34,6 +34,7 @@ private:
 
     juce::Slider gainSlider{ juce::Slider::SliderStyle::LinearVertical, juce::Slider::TextBoxBelow };
     juce::Label songNameLabel{ "Song Name" };
+    juce::Label artistNameLabel{ "Artist Name" };
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPlayerUI)
 };
 
